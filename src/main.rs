d8d32aa8973dfac4ff77b1bfa314e0dcd2b5de0a
Seth Adkins <seth.adkins@protonmail.com>
2025-08-12 11:39:13 -0400

    check for sleep status before doing anything

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,46 @@
 #![feature(generic_const_exprs)]
 
-use std::{io::Write, thread, time::Duration};
+use std::{io::{Read, Write}, thread, time::Duration};
 use serialport::{SerialPort, TTYPort};
 use sysinfo::{CpuRefreshKind, MemoryRefreshKind, RefreshKind, System};
 
-fn update_matrix(dev: &mut TTYPort, percent: u8) {
+fn get_sleep_status(dev: &mut TTYPort) -> Option<bool> {
 	let dev_path = dev.name().unwrap_or(String::new());
-	match dev.write(&[0x32, 0xAC, 0x01, 0x00, percent]) {
-		Ok(val) => println!("Wrote {} bytes to {}", val, &dev_path),
-		Err(e) => println!("Ran into error while writing to {}!: {}", &dev_path, e)
+	match dev.write(&[0x32, 0xAC, 0x03]) {
+		Ok(_) => (),
+		Err(e) => println!("Can't get sleep status of {}!: {}", &dev_path, e)
+	};
+	let mut response = [0u8; 1];
+	match dev.read(&mut response) {
+		Ok(val) if val > 0 => Some(response[0] != 0),
+		Ok(_) => None,
+		Err(e) => {
+			println!("Ran into problem reading from {}!: {}", &dev_path, e);
+			None
+		}
 	}
 }
+
+fn get_sleep_statuses(dev1: &mut TTYPort, dev2: &mut TTYPort) -> (Option<bool>, Option<bool>) {
+	(
+		match get_sleep_status(dev1) {
+			Some(val) => Some(val),
+			None => None
+		},
+		match get_sleep_status(dev2) {
+			Some(val) => Some(val),
+			None => None
+		}
+	)
+}
+
+fn update_matrix(dev: &mut TTYPort, percent: u8) {
+	match dev.write(&[0x32, 0xAC, 0x01, 0x00, percent]) {
+		Ok(_) => (),
+		Err(e) => println!("Ran into error while writing to {}!: {}", dev.name().unwrap_or(String::new()), e)
+	}
+}
+
 fn main() {
 	let mut sys = System::new_with_specifics(RefreshKind::nothing()
 		.with_cpu(CpuRefreshKind::nothing().with_cpu_usage())
@@ -19,10 +49,16 @@ fn main() {
 	let mut left_matrix = serialport::new("/dev/ttyACM1", 115200).open_native().expect("Can't open the given serial device!");
 	let mut right_matrix = serialport::new("/dev/ttyACM0", 115200).open_native().expect("Can't open the given serial device!");
 	loop {
-		sys.refresh_cpu_specifics(CpuRefreshKind::nothing().with_cpu_usage());
-		sys.refresh_memory_specifics(MemoryRefreshKind::nothing().with_ram());
-		update_matrix(&mut left_matrix, sys.global_cpu_usage() as u8);
-		update_matrix(&mut right_matrix, (sys.used_memory() as f64 / sys.total_memory() as f64 * 100.0) as u8);
+		if let (Some(left_sleeping), Some(right_sleeping)) = get_sleep_statuses(&mut left_matrix, &mut right_matrix) {
+			if !left_sleeping {
+				sys.refresh_cpu_specifics(CpuRefreshKind::nothing().with_cpu_usage());
+				update_matrix(&mut left_matrix, sys.global_cpu_usage() as u8);
+			}
+			if !right_sleeping {
+				sys.refresh_memory_specifics(MemoryRefreshKind::nothing().with_ram());
+				update_matrix(&mut right_matrix, (sys.used_memory() as f64 / sys.total_memory() as f64 * 100.0) as u8);
+			}
+		}
 		thread::sleep(Duration::from_secs(1));
 	}
 }
